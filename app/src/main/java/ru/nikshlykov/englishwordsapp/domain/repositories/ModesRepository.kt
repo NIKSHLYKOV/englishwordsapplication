087ccccb11b7e2ec49c973f2af3edb31e1f20175
Nik Shlykov <nikshlykovdev@gmail.com>
2021-08-08 17:24:40 +0300

    add methods to ModesRepository interface

--- app/src/main/java/ru/nikshlykov/englishwordsapp/domain/repositories/ModesRepository.kt
+++ app/src/main/java/ru/nikshlykov/englishwordsapp/domain/repositories/ModesRepository.kt
@@ -1,4 +1,12 @@
 package ru.nikshlykov.englishwordsapp.domain.repositories
 
+import ru.nikshlykov.englishwordsapp.db.mode.Mode
+
 interface ModesRepository {
+
+  suspend fun updateModes(modes: List<Mode>)
+
+  suspend fun getSelectedModes(): List<Mode>
+
+  suspend fun getAllModes(): List<Mode>
 }
