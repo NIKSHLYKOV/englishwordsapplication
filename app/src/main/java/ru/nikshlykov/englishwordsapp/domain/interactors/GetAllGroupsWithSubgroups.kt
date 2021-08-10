32b152d2f25bcbf0b2c3b57ff3d9e7ee00928530
Nik Shlykov <nikshlykovdev@gmail.com>
2021-08-10 13:38:34 +0300

    add some interactors for repeats, links and groups

---
+++ app/src/main/java/ru/nikshlykov/englishwordsapp/domain/interactors/GetAllGroupsWithSubgroups.kt
@@ -0,0 +1,27 @@
+package ru.nikshlykov.englishwordsapp.domain.interactors
+
+import ru.nikshlykov.englishwordsapp.db.subgroup.Subgroup
+import ru.nikshlykov.englishwordsapp.ui.GroupItem
+import java.util.*
+
+class GetAllGroupsWithSubgroups(
+  private val getGroupsInteractor: GetGroupsInteractor,
+  private val getSubgroupsFromGroupInteractor: GetSubgroupsFromGroupInteractor
+) {
+
+  suspend fun getAllGroupsWithSubgroups(): ArrayList<GroupItem> {
+    val groups = getGroupsInteractor.getAllGroups()
+    val groupItems = ArrayList<GroupItem>(groups.size)
+    for (group in groups) {
+      val subgroupsList = getSubgroupsFromGroupInteractor.getSubgroupsFromGroup(group.id)
+      // TODO посмотреть потом, можно ли переделать на List<>
+      val subgroups = ArrayList<Subgroup>(subgroupsList.size)
+      subgroups.addAll(subgroupsList)
+      if (subgroups.size != 0) {
+        val groupItem = GroupItem(group, subgroups)
+        groupItems.add(groupItem)
+      }
+    }
+    return groupItems
+  }
+}
