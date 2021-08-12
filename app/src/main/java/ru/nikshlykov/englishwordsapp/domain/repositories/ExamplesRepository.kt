7951f294ae7a603882f9ce122b5f3c2005c79992
Nik Shlykov <nikshlykovdev@gmail.com>
2021-08-12 17:36:38 +0300

    delete unnecessary code for examples

--- app/src/main/java/ru/nikshlykov/englishwordsapp/domain/repositories/ExamplesRepository.kt
+++ app/src/main/java/ru/nikshlykov/englishwordsapp/domain/repositories/ExamplesRepository.kt
@@ -3,6 +3,6 @@ package ru.nikshlykov.englishwordsapp.domain.repositories
 import ru.nikshlykov.englishwordsapp.db.example.Example
 
 interface ExamplesRepository {
-
+  // TODO Сделать фичу с примерами.
   suspend fun getExamplesByWordId(wordId: Long): List<Example>
 }
