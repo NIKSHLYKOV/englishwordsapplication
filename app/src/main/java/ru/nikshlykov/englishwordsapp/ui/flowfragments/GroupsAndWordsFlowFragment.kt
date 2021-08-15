c02ac9f2d1125244c1573a90da629da06da8f342
Nik Shlykov <nikshlykovdev@gmail.com>
2021-08-15 12:33:38 +0300

    delete unnecessary code

--- app/src/main/java/ru/nikshlykov/englishwordsapp/ui/flowfragments/GroupsAndWordsFlowFragment.kt
+++ app/src/main/java/ru/nikshlykov/englishwordsapp/ui/flowfragments/GroupsAndWordsFlowFragment.kt
@@ -30,15 +30,10 @@ class GroupsAndWordsFlowFragment : Fragment(), OnChildFragmentInteractionListene
 
   override fun onChildFragmentInteraction(navDirections: NavDirections?) {
     when (navDirections?.actionId) {
-      R.id.action_global_subgroup_data_dest,
       R.id.action_groups_dest_to_subgroup_dest,
       R.id.action_subgroup_dest_to_word_dest,
       R.id.action_subgroup_dest_to_add_word_dest ->
         navController!!.navigate(navDirections)
-      R.id.action_subgroup_data_dest_to_groups_dest,
-      R.id.action_subgroup_dest_to_groups_dest,
-      R.id.action_word_dest_to_subgroup_dest ->
-        navController!!.popBackStack()
     }
   } // TODO перехватывать нажатие кнопки назад, если на экране не GroupsFragment. Иначе передавать
 
