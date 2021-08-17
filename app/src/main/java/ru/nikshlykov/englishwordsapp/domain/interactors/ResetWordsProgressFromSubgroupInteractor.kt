b950e49aaa3ff03844b28a33c77ba8161a4d5b4a
Nik Shlykov <nikshlykovdev@gmail.com>
2021-08-17 23:51:41 +0300

    delete some TODO

--- app/src/main/java/ru/nikshlykov/englishwordsapp/domain/interactors/ResetWordsProgressFromSubgroupInteractor.kt
+++ app/src/main/java/ru/nikshlykov/englishwordsapp/domain/interactors/ResetWordsProgressFromSubgroupInteractor.kt
@@ -8,9 +8,4 @@ class ResetWordsProgressFromSubgroupInteractor(private val wordsRepository: Word
   suspend fun resetWordsProgressFromSubgroup(subgroupId: Long): Int {
     return wordsRepository.resetWordsProgressFromSubgroup(subgroupId)
   }
-  // TODO видимо, можно использовать тут интерактор для сброса прогресса по слову.
-
-  // TODO посмотреть потом, как мы проверяем, какой режим показывать слову.
-  //  Если не по прогрессу слова, то поменять, т.к. при сбрасывании прогресса
-  //  мы всё равно оставляем все повторы в БД.
 }
