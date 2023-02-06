7d208859655aef27d8069fb902e9d29112223eba
Nik Shlykov <nikshlykovdev@gmail.com>
2023-02-06 20:31:22 +0300

    add dependencies for compose and test screen

--- feature-statistics/src/main/java/ru/nikshlykov/feature_statistics/ui/fragments/StatisticsFragment.kt
+++ feature-statistics/src/main/java/ru/nikshlykov/feature_statistics/ui/fragments/StatisticsFragment.kt
@@ -1,6 +1,31 @@
 package ru.nikshlykov.feature_statistics.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
-import ru.nikshlykov.feature_statistics.R
 
-class StatisticsFragment : Fragment(R.layout.fragment_statistics)
+class StatisticsFragment : Fragment(){
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    return ComposeView(requireContext()).apply {
+      setContent {
+        StatisticsScreen()
+      }
+    }
+  }
+
+  @Composable
+  @Preview
+  fun StatisticsScreen(){
+    return Text(text = "Это фрагмент статистики")
+  }
+}
