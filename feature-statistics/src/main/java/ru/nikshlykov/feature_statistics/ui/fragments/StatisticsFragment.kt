8a8ccb77942597072a90c95cfe0075e6adf9b635
shlykov-nik <shlykov-nik@yandex-team.ru>
2025-02-22 16:55:05 +0300

    EWA-250222-1 change compose version

--- feature-statistics/src/main/java/ru/nikshlykov/feature_statistics/ui/fragments/StatisticsFragment.kt
+++ feature-statistics/src/main/java/ru/nikshlykov/feature_statistics/ui/fragments/StatisticsFragment.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
